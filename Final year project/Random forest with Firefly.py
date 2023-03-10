0627ba8109702a78a9498fa7984bec409f28865e
Lisha Narmada Murala <89591360+lishanarmada@users.noreply.github.com>
2023-03-10 11:55:08 +0530

    Add files via upload

---
+++ Final year project/Random forest with Firefly.py
@@ -0,0 +1,44 @@
+pip install sklearn-nature-inspired-algorithms
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(random_state=42)
+param_grid = {
+   'n_estimators': range(10, 80, 20),
+   'max_depth': [2, 4, 6, 8, 10, 20],
+   'min_samples_split': range(2, 8, 2),
+   'max_features': ["auto", "sqrt", "log2"]
+}
+import pandas as pd
+import numpy as np
+data = pd.read_csv('/content/MSCAD.csv')
+X = data.iloc[:,:-1]
+print("The attributes are:",X)
+y = data.iloc[:,-1]
+print("The target is:",y)
+from sklearn.preprocessing import LabelEncoder
+le_output = LabelEncoder()
+y.output = le_output.fit_transform(y)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(f'train size - {len(X_train)}\ntest size - {len(X_test)}')
+from sklearn_nature_inspired_algorithms.model_selection import NatureInspiredSearchCV
+nia_search = NatureInspiredSearchCV(
+    clf,
+    param_grid,
+    cv=5,
+    verbose=1,
+    algorithm='fa', 
+    population_size=25,
+    max_n_gen=100,
+    max_stagnating_gen=10,    
+    runs=5,
+    scoring='f1_macro',
+    random_state=42,
+)
+
+nia_search.fit(X_train, y_train)
+print(nia_search.best_params_)
+from sklearn.metrics import classification_report
+clf = RandomForestClassifier(**nia_search.best_params_, random_state=42)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print(classification_report(y_test, y_pred, digits=4))
